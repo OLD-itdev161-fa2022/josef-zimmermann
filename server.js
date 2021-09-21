85a531b4d9eb67d200a0ce6ba0ef60eb0cf39411
Josef Zimmermann <zimmej26@gmatc.matc.edu>
2021-09-21 17:45:24 -0500

    updated assignment-3

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false}));
+
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log(`express server running on port 3000`));
 
